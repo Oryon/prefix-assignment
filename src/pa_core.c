2eac64e0bb76c94d02809068064104c5c243046e
Oryon <pierre@darou.fr>
2014-12-09 15:48:58 +0100

    Adding API plus structure functions

--- src/pa_core.c
+++ src/pa_core.c
@@ -7,4 +7,239 @@
 
 #include "pa_core.h"
 
+#include <stdlib.h>
+#include <string.h>
 
+#ifndef container_of
+#define container_of(ptr, type, member) (           \
+    (type *)( (char *)ptr - offsetof(type,member) ))
+#endif
+
+/* Returns whether the Advertised Prefix takes precedence over the Assigned Prefix. */
+#define pa_precedes(pp, ap) \
+	((!ap->published) || ((pp)->priority > (ap)->priority) || \
+	(((pp)->priority == (ap)->priority) && memcmp((pp)->node_id, (ap)->core->node_id, PA_NODE_ID_LEN)))
+
+#define pa_for_each_ap_in_dp_safe(pa_dp, pa_ap, pa_ap2) list_for_each_entry_safe(pa_ap, pa_ap2, &(pa_dp)->aps, in_dp)
+#define pa_for_each_ap_in_link_safe(pa_link, pa_ap, pa_ap2) list_for_each_entry_safe(pa_ap, pa_ap2, &(pa_link)->aps, in_link)
+#define pa_for_each_user(pa_core, pa_user) list_for_each_entry(pa_user, &(pa_core)->users, le)
+
+#define pa_user_notify(pa_ap, function) \
+	do { \
+		struct pa_user *_pa_user_notify_user; \
+		pa_for_each_user((pa_ap)->core, _pa_user_notify_user) { \
+			if(_pa_user_notify_user->function) \
+				_pa_user_notify_user->function(_pa_user_notify_user, ap);\
+		} \
+	} while(0)
+
+#define pa_ap_set_published(pa_ap, p) do {\
+	if((pa_ap)->published != p) { \
+		(pa_ap)->published = p; \
+		pa_user_notify(pa_ap, published); \
+	} }while(0)
+
+#define pa_ap_set_applied(pa_ap, p) do {\
+	if((pa_ap)->applied != p) { \
+		(pa_ap)->applied = p; \
+		pa_user_notify(pa_ap, applied); \
+	} }while(0)
+
+#define pa_routine_schedule(ap) do { \
+	if(!(ap)->routine_to.pending) \
+		uloop_timeout_set(&(ap)->routine_to, PA_RUN_DELAY); }while(0)
+
+static void pa_ap_unassign(struct pa_ap *ap)
+{
+	struct pa_ap *ap2;
+	if(!ap->assigned)
+		return;
+
+	btrie_remove(&ap->in_core.be);
+	ap->assigned = 0;
+
+	pa_user_notify(ap, applied); /* Tell users about that */
+
+	/* Destroying the Assigned Prefix possibly freed space that other interfaces may use.
+	 * Schedule links for the same dp, if there is no current prefix.
+	 * This can be ignored when no prefix is ever created by the local node. */
+	pa_for_each_ap_in_dp(ap->dp, ap2) {
+		if(!ap2->assigned)
+			pa_routine_schedule(ap2);
+	}
+}
+
+static int pa_ap_assign(struct pa_ap *ap, const struct in6_addr *prefix, uint8_t plen)
+{
+	if(ap->assigned)
+		return -2;
+
+	memcpy(&ap->prefix, prefix, sizeof(struct in6_addr));
+	ap->plen = plen;
+	if(btrie_add(&ap->core->prefixes, &ap->in_core.be, (const btrie_key_t *)prefix, plen))
+		return -1;
+
+	ap->assigned = 1;
+	pa_user_notify(ap, assigned); /* Tell users about that*/
+	return 0;
+}
+
+/*
+ * Prefix Assignment Routine.
+ */
+static void pa_routine(struct pa_ap *ap, bool backoff)
+{
+
+}
+
+static void pa_backoff_to(struct uloop_timeout *to)
+{
+	struct pa_ap *ap = container_of(to, struct pa_ap, backoff_to);
+	if(ap->published) {
+		//Apply timeout
+		pa_ap_set_applied(ap, 1);
+	} else if(ap->assigned) {
+		//Adopt timeout
+		//TODO
+	} else {
+		//Creation delay
+		pa_routine(ap, true);
+	}
+}
+
+static void pa_routine_to(struct uloop_timeout *to)
+{
+	struct pa_ap *ap = container_of(to, struct pa_ap, routine_to);
+	pa_routine(ap, false);
+}
+
+/*
+ * Create a new empty link/dp pairing.
+ */
+static int pa_ap_create(struct pa_core *core, struct pa_link *link, struct pa_dp *dp)
+{
+	struct pa_ap *ap;
+	if(!(ap = calloc(1, sizeof(*ap))))
+		return -1;
+
+	ap->backoff_to.cb = pa_backoff_to;
+	ap->routine_to.cb = pa_routine_to;
+	ap->in_core.type = PAT_AP;
+	ap->core = core;
+	ap->link = link;
+	list_add(&ap->in_link, &link->aps);
+	ap->dp = dp;
+	list_add(&ap->in_dp, &dp->aps);
+	return 0;
+}
+
+/*
+ * Destroy an ap. All states must be to 0.
+ */
+static void pa_ap_destroy(struct pa_ap *ap)
+{
+	list_del(&ap->in_link);
+	list_del(&ap->in_dp);
+	uloop_timeout_cancel(&ap->backoff_to);
+	uloop_timeout_cancel(&ap->routine_to);
+	free(ap);
+}
+
+void pa_dp_del(struct pa_dp *dp)
+{
+	struct pa_ap *ap, *ap2;
+	//Public part
+	pa_for_each_ap_in_dp(dp, ap) {
+		pa_ap_set_published(ap, 0);
+		pa_ap_set_applied(ap, 0);
+		pa_ap_unassign(ap);
+	}
+
+	//Private part (so the whole deletion is atomic for users)
+	pa_for_each_ap_in_dp_safe(dp, ap, ap2)
+		pa_ap_destroy(ap);
+	list_del(&dp->le);
+}
+
+int pa_dp_add(struct pa_core *core, struct pa_dp *dp)
+{
+	struct pa_link *link;
+	pa_for_each_link(core, link) {
+		if(pa_ap_create(core, link, dp)) {
+			pa_dp_del( dp);
+			return -1;
+		}
+	}
+	list_add(&dp->le, &core->dps);
+	return 0;
+}
+
+void pa_link_del(struct pa_link *link)
+{
+	struct pa_ap *ap, *ap2;
+	//Public part
+	pa_for_each_ap_in_link(link, ap) {
+		pa_ap_set_published(ap, 0);
+		pa_ap_set_applied(ap, 0);
+		pa_ap_unassign(ap);
+	}
+
+	//Private part (so the whole deletion is atomic for users)
+	pa_for_each_ap_in_link_safe(link, ap, ap2)
+		pa_ap_destroy(ap);
+
+	list_del(&link->le);
+}
+
+int pa_link_add(struct pa_core *core, struct pa_link *link)
+{
+	struct pa_dp *dp;
+	pa_for_each_dp(core, dp) {
+		if(pa_ap_create(core, link, dp)) {
+			pa_link_del(link);
+			return -1;
+		}
+	}
+	list_add(&link->le, &core->links);
+	return 0;
+}
+
+void pa_core_set_flooding_delay(struct pa_core *core, uint32_t flooding_delay)
+{
+	struct pa_link *link;
+	struct pa_ap *ap;
+	if(flooding_delay > core->flooding_delay) {
+		pa_for_each_link(core, link)
+			pa_for_each_ap_in_link(link, ap)
+				if(ap->published && ap->backoff_to.pending)
+					uloop_timeout_set(&ap->backoff_to, uloop_timeout_remaining(&ap->backoff_to) + 2*(flooding_delay - core->flooding_delay));
+	} else if (flooding_delay < core->flooding_delay) {
+		pa_for_each_link(core, link)
+			pa_for_each_ap_in_link(link, ap)
+				if(ap->published && ap->backoff_to.pending && ((uint32_t)uloop_timeout_remaining(&ap->backoff_to) > 2*flooding_delay))
+					uloop_timeout_set(&ap->backoff_to, 2*flooding_delay);
+	}
+	core->flooding_delay = flooding_delay;
+}
+
+void pa_core_set_node_id(struct pa_core *core, const uint8_t node_id[])
+{
+	struct pa_link *link;
+	struct pa_ap *ap;
+	if(memcmp(node_id, core->node_id, PA_NODE_ID_LEN)) {
+		memcpy(core->node_id, node_id, PA_NODE_ID_LEN);
+		/* Schedule routine for all pairs */
+		pa_for_each_link(core, link)
+			pa_for_each_ap_in_link(link, ap)
+				pa_routine_schedule(ap);
+	}
+}
+
+void pa_core_init(struct pa_core *core)
+{
+	INIT_LIST_HEAD(&core->dps);
+	INIT_LIST_HEAD(&core->links);
+	INIT_LIST_HEAD(&core->users);
+	btrie_init(&core->prefixes);
+	memset(core->node_id, 0, PA_NODE_ID_LEN);
+}
--- src/pa_core.h
+++ src/pa_core.h
@@ -5,11 +5,241 @@
  *
  * Implementation of the Distributed Prefix Assignment Algorithm.
  *
+ * The algorithm is specified as an IETF Internet Draft:
+ * https://tools.ietf.org/html/draft-ietf-homenet-prefix-assignment
+ *
  */
 
 #ifndef PA_CORE_H_
 #define PA_CORE_H_
 
+#include <netinet/in.h>
+#include <stdint.h>
 
+#include <libubox/list.h>
+#include <libubox/uloop.h>
+
+#include "btrie.h"
+
+
+/***************************
+ *    Global parameters.   *
+ ***************************/
+
+/* Logging functions used by all pa files. */
+#ifndef PA_WARNING
+#define PA_WARNING(format, ...)
+#endif
+#ifndef PA_LOG
+#define PA_LOG(format, ...)
+#endif
+
+/* Length, in byte, of a Node ID. */
+#define PA_NODE_ID_LEN 8
+
+/* Advertised Prefix Priority type. */
+typedef uint8_t pa_priority;
+
+/* Internal rule priority type. */
+typedef uint16_t pa_rule_priority;
+
+/* Delay, in milliseconds, between the events triggering
+ * the prefix assignment routine and the actual time it is run.
+ * The routine is never run synchronously, even when the delay is set to 0. */
+#define PA_RUN_DELAY 20
+
+
+
+/***************************
+ *       Generic API       *
+ ***************************/
+
+/* Structure containing state specific to the overall algorithm. */
+struct pa_core {
+	struct btrie prefixes;           /* btrie containing all Assigned and Advertised Prefixes */
+	uint8_t node_id[PA_NODE_ID_LEN]; /* The Node ID of the local node. Initial value is 0. */
+	uint32_t flooding_delay;         /* The Flooding Delay. Initial value is 10 seconds. */
+	struct list_head users;
+	struct list_head links;
+	struct list_head dps;
+};
+
+
+/* Initializes a pa_core structure.
+ * Once initialized, it can be configured with rules, prefixes and links.
+ * Returns 0 upon success and -1 otherwise.
+ */
+void pa_core_init(struct pa_core *core);
+
+/* Sets the local node ID.
+ * This operation run or schedule
+ */
+void pa_core_set_node_id(struct pa_core *core, const uint8_t[PA_NODE_ID_LEN]);
+
+/* Sets the flooding delay to the specified value.
+ * When the delay is increased, all running timers are increased by old_flooding_delay - new_flooding_delay
+ * When the delay is decreased, running timers are set to min(remaining, new_flooding_delay)
+ * flooding_delay must be smaller than 2 << 31 because it is multiplied by 2 in a uint32_t.
+ */
+void pa_core_set_flooding_delay(struct pa_core *core, uint32_t flooding_delay);
+
+
+
+/*
+ * Structure used to identify a Shared or Private Link.
+ */
+struct pa_link {
+	struct list_head le;
+	struct list_head aps;
+};
+
+/* Adds and deletes a Link for prefix assignment */
+int pa_link_add(struct pa_core *, struct pa_link *);
+void pa_link_del(struct pa_link *);
+
+/*
+ * Structure used to identify a Delegated Prefix.
+ */
+struct pa_dp {
+	struct list_head le;
+	struct list_head aps;
+	struct in6_addr prefix;
+	uint8_t plen;
+};
+
+/* Adds and deletes a Delegated Prefix for prefix assignment */
+int pa_dp_add(struct pa_core *, struct pa_dp *);
+void pa_dp_del(struct pa_dp *);
+
+/*
+ * Structure used to link all prefixes in the same tree.
+ */
+struct pa_pentry {
+	struct btrie_element be; /* The btrie element. */
+	uint8_t type;            /* Prefix type. */
+#define PAT_AP 0x01
+#define PAT_PP 0x02
+};
+
+/*
+ * Structure used to identify a Link/Delegated Prefix pair.
+ * It may or may not contain an Assigned Prefix.
+ */
+struct pa_ap {
+	struct pa_pentry in_core;       /* Used to link aps and pps in the same btrie */
+	struct list_head in_link;       /* Linked in the link structure. */
+	struct list_head in_dp;         /* Linked in the Delegated Prefix structure. */
+	struct pa_link *link;           /* The Link associated with the AP. */
+	struct pa_dp *dp;               /* The DP associated with the AP. */
+	struct pa_core *core;           /* Back-pointer to the associated pa_core struct */
+	uint8_t assigned  : 1;          /* There is an associated Assigned Prefix. */
+	uint8_t published : 1;          /* The AP is published. */
+	uint8_t applied   : 1;          /* The AP is applied. */
+	struct in6_addr prefix;         /* (if assigned) The AP prefix. */
+	uint8_t plen;                   /* (if assigned) The AP prefix length. */
+	pa_priority priority;           /* (if published) The Advertised Prefix Priority. */
+	pa_rule_priority rule_priority; /* (if published) The internal rule priority. */
+	struct pa_rule *rule;           /* (if published) The rule used to publish this prefix. */
+	struct uloop_timeout routine_to;/* Timer used to schedule the routine. */
+	struct uloop_timeout backoff_to;/* Timer used to backoff prefix generation, adoption or apply. */
+	struct pa_pp *best_assignment;  /* (when in routine) The best current assignment. */
+};
+
+/*
+ * Structure used to identify an Advertised Prefix.
+ */
+struct pa_pp {
+	struct pa_pentry in_core;     /* Used to link aps and pps in the same btrie */
+	uint8_t node_id[PA_NODE_ID_LEN]; /* The node ID of the node advertising the prefix. */
+	struct in6_addr prefix;       /* The Advertised Prefix). */
+	uint8_t plen;                 /* The Advertised Prefix length. */
+	pa_priority priority;         /* The Advertised Prefix Priority. */
+	struct pa_link *link;         /* Advertised Prefix associated Shared Link (or null). */
+};
+
+/* Adds a new Advertised Prefix. */
+int pa_pp_add(struct pa_core *, struct pa_pp *);
+
+/* Removes an added Advertised Prefix. */
+int pa_pp_del(struct pa_core *, struct pa_pp *);
+
+/* Tell the content of the Advertised Prefix was changes. */
+int pa_pp_update(struct pa_core *, struct pa_pp *);
+
+
+/***************************
+ *         User API        *
+ ***************************/
+
+struct pa_user {
+	struct list_head le; /* Linked in pa_core. */
+
+	/* These callbacks are called when the assigned, published
+	 * and applied values are changed. They are not called
+	 * when a Link/DP is created.
+	 * When switched to 0, associated values are still present.
+	 * i.e. the prefix and plen are valid when assigned == 0
+	 * i.e. the priorities and rule are still valid when published == 0 */
+	void (*assigned)(struct pa_user *, struct pa_ap *);
+	void (*published)(struct pa_user *, struct pa_ap *);
+	void (*applied)(struct pa_user *, struct pa_ap *);
+};
+
+/* Adds a user which will receive events callback.
+ * When added, the user does not receive callbacks for existing prefixes. */
+#define pa_user_register(core, user) list_add(&(core)->users, &(user)->le)
+
+/* Unregister a user. */
+#define pa_user_unregister(user) list_del(&(user)->le)
+
+#define pa_for_each_link(pa_core, pa_link) list_for_each_entry(pa_link, &(pa_core)->links, le)
+
+#define pa_for_each_ap_in_link(pa_link, pa_ap) list_for_each_entry(pa_ap, &(pa_link)->aps, in_link)
+
+#define pa_for_each_dp(pa_core, pa_dp) list_for_each_entry(pa_dp, &(pa_core)->dps, le)
+
+#define pa_for_each_ap_in_dp(pa_dp, pa_ap) list_for_each_entry(pa_ap, &(pa_dp)->aps, in_dp)
+
+
+/***************************
+ *   Configuration API     *
+ ***************************/
+
+/* Argument passed to a rule. */
+struct pa_rule_arg {
+	/* The action to be done. */
+	uint8_t action; //DESTROY, KEEP, CHANGE
+	uint32_t backoff_timer;
+	uint32_t apply_timer;
+};
+
+/* */
+struct pa_rule_prefix_count {
+
+};
+
+/* This structure is a raw structure for prefix selection.
+ * Specific rules and API are defined in pa_rules.h. */
+struct pa_rule {
+	struct list_head le; /* Linked in pa_core, the Link, or the DP. */
+
+	/* Called whenever a rule is evaluated for a given Link/DP pair.
+	 * Must return 0 when matching, a non-null value otherwise.
+	 * When matching, the pa_rule_arg argument must be filled. */
+	int (*apply_rule)(struct pa_rule *, struct pa_ap *, struct pa_rule_arg *);
+
+	pa_rule_priority max_rule_priority;
+	pa_rule_priority max_rule_override_priority;
+	pa_priority max_priority;
+	pa_priority max_override_priority;
+};
+
+void pa_rule_add(struct pa_core *, struct pa_rule *);
+void pa_rule_del(struct pa_core *, struct pa_rule *);
+
+/* Allows a rule to access the prefix count array.
+ * This must not be computed by rules individually in order to avoid
+ * redundant computations. */
+const struct pa_rule_prefix_count *pa_rule_prefix_count(struct pa_core *, struct pa_rule_arg *);
 
 #endif /* PA_CORE_H_ */
